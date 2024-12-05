e7bb5aea15dc6207ea90edfa87f7d29cbd6f9668
k042330 <126170992+k042330@users.noreply.github.com>
2024-12-06 05:20:00 +0800

    Add files via upload

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,17 +1,17 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist',
+    outDir: 'dist'
   },
   server: {
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
-        changeOrigin: true,
-      },
-    },
-  },
-});
+        changeOrigin: true
+      }
+    }
+  }
+})
